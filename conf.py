afc8d05f8d44d6b8fb1f8d3e2d3f3039b56a761d
OscarMaestre <profesor.oscar.gomez@gmail.com>
2014-09-23 12:08:41 +0200

    Actualizado el PDF a 19-9-2014

--- conf.py
+++ conf.py
@@ -197,7 +197,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'Moviles.tex', 'Programación de Servicios y Procesos',
+  ('index', 'Moviles.tex', 'Programación de Móviles',
    'Oscar Gomez', 'manual'),
 ]
 
